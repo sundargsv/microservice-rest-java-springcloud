15eb47ca826f9825fc58550dfe8e015614381328
Gnana Sundaram Ganesa Sivam <gnanasundaram.ganesasivam@centurylink.com>
2019-08-18 13:21:53 +0530

    Changes to the configuration parameters and added TokenConfig to control

--- auth/src/main/java/com/sundar/microservices/auth/configuration/WebSecurityConfig.java
+++ auth/src/main/java/com/sundar/microservices/auth/configuration/WebSecurityConfig.java
@@ -48,7 +48,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // We don't need CSRF for this example
         httpSecurity.csrf().disable()
                 // dont authenticate this particular request
-                .authorizeRequests().antMatchers(tokenConfig.getUri()).permitAll().
+                .authorizeRequests().antMatchers(tokenConfig.getUri().split(",")).permitAll().
                 // all other requests need to be authenticated
                         anyRequest().authenticated().and().
                 // make sure we use stateless session; session won't be used to
